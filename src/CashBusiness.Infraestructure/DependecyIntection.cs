5e14a8011a0156d8c6dedef6ab9747c3d48d3d4f
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-19 12:08:24 -0400

    chore: operation repository service added to application scope

--- src/CashBusiness.Infraestructure/DependecyIntection.cs
+++ src/CashBusiness.Infraestructure/DependecyIntection.cs
@@ -1,5 +1,7 @@
+using CashBusiness.Application.Common.Persistence;
 using Microsoft.EntityFrameworkCore;
 using CashBusiness.Infraestructure.Persistence;
+using CashBusiness.Infraestructure.Persistence.Transaction;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CashBusiness.Infraestructure;
@@ -8,8 +10,11 @@ public static class DependecyIntection
 {
     public static  IServiceCollection AddInfraestructure(this IServiceCollection services){
         services.AddDbContext<AppDbContext>(
-            options => options.UseSqlServer("Server=DESKTOP-2TTGTQJ;Database=pruebabasedatos;Integrated Security=True; TrustServerCertificate=True",
+            options => options.UseSqlServer("Server=DESKTOP-2TTGTQJ;Database=SisCashBusinessDB;Integrated Security=True; TrustServerCertificate=True",
                 b => b.MigrationsAssembly("CashBusiness.Api")));
+
+        services.AddScoped<IOperationRepository, OperationRepositoryImpl>();
+        
         return services;
 
     }
