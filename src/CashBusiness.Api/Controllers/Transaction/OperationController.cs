78bd5eaf9715484e5d44e94c3598f5a6004488c0
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-19 12:28:09 -0400

    feat: find by id and find all operations methods added

---
+++ src/CashBusiness.Api/Controllers/Transaction/OperationController.cs
@@ -0,0 +1,26 @@
+﻿using CashBusiness.Application.Services.Transaction;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashBusiness.Api.Controllers.Transaction;
+
+[ApiController]
+[Route("operation")]
+public class OperationController: ControllerBase
+{
+    private readonly IOperationService _operationService;
+    
+    public OperationController(IOperationService operationService)
+    {
+        _operationService = operationService;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        var result = await _operationService.findAll();
+        return Ok(result);
+    }
+    
+    
+    
+}
