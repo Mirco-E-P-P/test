9bfcc98c3cad62477cfb2276f9a74531a757ac4a
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-19 12:15:19 -0400

    chore: operation service methods implemented

--- src/CashBusiness.Application/Services/Transaction/OperationServiceImpl.cs
+++ src/CashBusiness.Application/Services/Transaction/OperationServiceImpl.cs
@@ -1,6 +1,26 @@
-﻿namespace CashBusiness.Application.Services.Transaction;
+﻿using CashBusiness.Application.Common.Persistence;
+using CashBusiness.Domain.Entity;
 
-public class OperationServiceImpl
+namespace CashBusiness.Application.Services.Transaction;
+
+public class OperationServiceImpl: IOperationService
 {
     
+    private readonly IOperationRepository _operationRepository;
+    
+    public OperationServiceImpl(IOperationRepository operationRepository)
+    {
+        _operationRepository = operationRepository;
+    }
+    
+    
+    public async Task<List<Operation>> findAll()
+    {
+        return await _operationRepository.findAll();
+    }
+
+    public async Task<Operation> findById(string id)
+    {
+        return await _operationRepository.findById(id);
+    }
 }
