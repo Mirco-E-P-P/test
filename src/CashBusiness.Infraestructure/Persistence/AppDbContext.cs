91315c4a1f7618d66e1af82f58b211e64b8e6a21
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-19 12:01:11 -0400

    chore: operation and concept entities added to db context

--- src/CashBusiness.Infraestructure/Persistence/AppDbContext.cs
+++ src/CashBusiness.Infraestructure/Persistence/AppDbContext.cs
@@ -5,21 +5,18 @@ namespace CashBusiness.Infraestructure.Persistence;
 
 public class AppDbContext : DbContext
 {
-
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        modelBuilder.Entity<Concept>().HasKey(concept => concept.id);
-        //modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        modelBuilder.Entity<Concept>().HasKey(concept => concept.Id);
+        modelBuilder.Entity<Operation>().HasIndex(operation => operation.Id);
+        
     }
-    // public DbSet<User> Users { get; set; }
-    // public DbSet<CashTransaction> CashTransactions { get; set; }
-    // public DbSet<Operation> Operations { get; set; }
-    public DbSet<Concept> Concepts { get; set; }
-    // public DbSet<Currency> Currencies { get; set; }
 
+    public DbSet<Operation> Operations { get; set; }
+    public DbSet<Concept> Concepts { get; set; }
 }
 
 
