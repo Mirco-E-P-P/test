50a70b454d2e9219c1e1fdd83efd33c4ebae0ade
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-19 12:17:51 -0400

    chore: operaion service and repository to dependency injection

--- src/CashBusiness.Api/Program.cs
+++ src/CashBusiness.Api/Program.cs
@@ -1,3 +1,4 @@
+using CashBusiness.Application;
 using CashBusiness.Infraestructure;
 using CashBusiness.Infraestructure.Persistence;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddInfraestructure();
+builder.Services.AddApplication();
 
 
 
