c4f376d3d4f3fc7c5353ece78a917d77237eea2d
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-20 08:55:17 -0400

    chore: application unitary tests added

---
+++ tests/UnitTests/CashBusiness.Application.UnitTests/Services/Transaction/OperationServiceTests.cs
@@ -0,0 +1,72 @@
+﻿using CashBusiness.Application.Common.Persistence;
+using CashBusiness.Application.UnitTests.Services.Transaction.ServiceUtils;
+using CashBusiness.Domain.Entity;
+using CashBusiness.Infraestructure.Persistence;
+using CashBusiness.Infraestructure.Persistence.Transaction;
+
+using Moq;
+
+
+namespace CashBusiness.Application.UnitTests.Services.Transaction;
+using CashBusiness.Application.Services.Transaction;
+
+public class OperationServiceTests
+{
+    
+    private readonly Mock<IOperationRepository> _mockOperationRepository;  
+    private readonly IOperationService _operationService;
+      
+    
+    public OperationServiceTests()
+    {
+        this._mockOperationRepository = new Mock<IOperationRepository>();
+        this._operationService = new OperationServiceImpl(_mockOperationRepository.Object);
+    }
+    
+    [Fact]
+    public async Task GetOperationsAsync_ReturnsOperations()
+    {
+        Operation operation = new Operation( );
+        operation.Id = Guid.NewGuid();
+        operation.Name = "Ingreso";
+        
+        
+        // Arrange
+        
+        
+        _mockOperationRepository.Setup(
+                x => x.findAll()
+                )
+            .ReturnsAsync(new List<Operation> {operation});
+
+        // Act
+        var result = await _operationService.findAll();
+
+        // Assert
+        Assert.Single(result);
+    }
+
+
+    [Fact]
+    public async Task GetOperationAsync_TwoOperationsInDatabase_shouldReturnTwoRecords()
+    {
+        AppDbContext dbContext = InMemoryApplicationDatabase.GetInstance().GetApplicationDbContext();
+ 
+        dbContext.Operations.Add(new Operation { Id = Guid.NewGuid(), Name = "Ingresosss" });
+        dbContext.Operations.Add(new Operation { Id = Guid.NewGuid(), Name = "Egtrsosadasdas" });
+        dbContext.SaveChanges();
+        
+        var repository = new OperationRepositoryImpl(dbContext);
+        var service = new OperationServiceImpl(repository);
+        
+        var operations = await service.findAll();
+        
+        Assert.NotNull(operations);
+        Assert.Equal(2, operations.Count()); 
+    }
+
+
+
+
+
+}
