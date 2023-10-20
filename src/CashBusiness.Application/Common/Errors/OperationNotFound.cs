d9e56d412b14a55ceb10a4370f63116c3e20d78b
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-20 09:51:02 -0400

    chore: not found operation error created

---
+++ src/CashBusiness.Application/Common/Errors/OperationNotFound.cs
@@ -0,0 +1,10 @@
+﻿using FluentResults;
+
+namespace CashBusiness.Application.Common.Errors;
+
+public class OperationNotFound: IError
+{
+    public string Message { get; } = "Operation not found";
+    public Dictionary<string, object> Metadata { get; }
+    public List<IError> Reasons { get; }
+}
