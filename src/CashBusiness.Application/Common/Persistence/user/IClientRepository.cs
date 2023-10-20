4f0ccff2fc2904de73bef2ea21eee7810451e696
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-20 14:27:10 -0400

    chore: client repository interface added

--- src/CashBusiness.Application/Common/Persistence/user/IClientRepository.cs
+++ src/CashBusiness.Application/Common/Persistence/user/IClientRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace CashBusiness.Application.Common.Persistence.user;
+﻿using CashBusiness.Domain.Entity;
+
+namespace CashBusiness.Application.Common.Persistence.user;
 
 public interface IClientRepository
 {
-    
+    public Task<Client> persistedClient(string name, string phoneNumber);
+    public Task<Client> findClientById(string id);
+    public Task<List<Client>> findAllClients();
+
 }
