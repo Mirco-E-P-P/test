1113d7d836bfa3f5e97461369401a1d0c80cbca7
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-20 13:43:10 -0400

    chore: client entity configuration added

---
+++ src/CashBusiness.Infraestructure/Common/DbContext/config/ClientConfig.cs
@@ -0,0 +1,15 @@
+﻿using CashBusiness.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CashBusiness.Infraestructure.config;
+
+public class ClientConfig: IEntityTypeConfiguration<Client>
+{
+    public void Configure(EntityTypeBuilder<Client> builder)
+    {
+        builder.HasKey(client => client.Id);
+        builder.Property(client => client.Name).IsRequired();
+        builder.Property(client => client.PhoneNumber).HasMaxLength(30);
+    }
+}
