cbf78b076761ed2c25226086f9881c2c8983c462
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 10:13:01 -0400

    refactor: register client dto change to register customer dto

--- src/CashBusiness.Contracts/User/CreateCustomerDto.cs
+++ src/CashBusiness.Contracts/User/CreateCustomerDto.cs
@@ -1,6 +1,6 @@
 ﻿namespace CashBusiness.Contracts.User;
 
-public class CreateClientDto
+public class CreateCustomer
 {
     public string Name { get; set; } = String.Empty;
     public string PhoneNumber { get; set; } = String.Empty;
