e87fe1806ff8f98fe3ee9e3edc719b662fb6b8dd
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 10:16:16 -0400

    refactor: client repository change to customer repository

---
+++ src/CashBusiness.Application/Common/Persistence/user/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using CashBusiness.Domain.Entity;
+
+namespace CashBusiness.Application.Common.Persistence.user;
+
+public interface IClientRepository
+{
+    public Task<Customer> PersistedClient(string name, string phoneNumber);
+    public Task<Customer> FindClientById(string id);
+    public Task<List<Customer>> FindAllClients();
+    public Task<Customer> FindClientByName(string name);
+
+}
