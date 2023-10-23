868ea270025ef3721e8dc9f42866fb3c6aababea
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 12:24:29 -0400

    chore: customer entity mapping configuration added

---
+++ src/CashBusiness.Api/Commons/Mapping/Config/CustomerMappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using CashBusiness.Contracts.User.vo;
+using CashBusiness.Domain.Entity;
+using Mapster;
+
+namespace CashBusiness.Api.Commons.Mapping.Config;
+
+public class CustomerMappingConfig: IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<Customer, CustomerVo>()
+            .Map(destination => destination.Id, source => source.Id.ToString())
+            .Map(destination => destination.Name, source => source.Name)
+            .Map(destination => destination.PhoneNumber, source => source.PhoneNumber);
+        
+    }
+}
