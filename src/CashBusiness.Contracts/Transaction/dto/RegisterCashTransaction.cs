8be1bfcaa3c008c3fa1f46d301a14bdf747c62b4
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 12:42:30 -0400

    chore: register cash transaction dto added

---
+++ src/CashBusiness.Contracts/Transaction/dto/RegisterCashTransaction.cs
@@ -0,0 +1,10 @@
+﻿namespace CashBusiness.Contracts.Transaction.dto;
+
+public class RegisterCashTransaction
+{
+    public string ClientId { get; set; } = String.Empty;
+    public string Voucher { get; set; } = String.Empty;
+    public string OperationId { get; set; } = String.Empty;
+    public double Amount { get; set; } = 0.0;
+    public string Observation { get; set; } = String.Empty;
+}
