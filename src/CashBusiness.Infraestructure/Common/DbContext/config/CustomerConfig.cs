e3424b8aae24ad0f50a066378dcbd974e2cb27f1
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 11:58:38 -0400

    chore: the index and id fields are the composite key of customer entity

--- src/CashBusiness.Infraestructure/Common/DbContext/config/CustomerConfig.cs
+++ src/CashBusiness.Infraestructure/Common/DbContext/config/CustomerConfig.cs
@@ -8,7 +8,9 @@ public class CustomerConfig: IEntityTypeConfiguration<Customer>
 {
     public void Configure(EntityTypeBuilder<Customer> builder)
     {
-        builder.HasKey(customer => customer.Id);
+        builder.HasKey(nameof(Customer.Index), nameof(Customer.Id));
+        builder.Property(customer => customer.Index).ValueGeneratedOnAdd();
+        
         builder.Property(customer => customer.Name).IsRequired();
         builder.Property(customer => customer.PhoneNumber).HasMaxLength(30);
     }
