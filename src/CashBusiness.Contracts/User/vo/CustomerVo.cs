294f982c3f8d518adbdaf4302368aeddeabf3a9c
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 15:04:39 -0400

    refactor: default guid value removed from customer response

--- src/CashBusiness.Contracts/User/vo/CustomerVo.cs
+++ src/CashBusiness.Contracts/User/vo/CustomerVo.cs
@@ -2,7 +2,7 @@
 
 public class CustomerVo
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string PhoneNumber { get; set; } = string.Empty;
 }
