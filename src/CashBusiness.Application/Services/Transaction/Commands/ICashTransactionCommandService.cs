75eba504a28c954ac5dca9b25577de1848eb2a1c
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 12:59:16 -0400

    chore: cash transaction command service interface methods declared

---
+++ src/CashBusiness.Application/Services/Transaction/Commands/ICashTransactionCommandService.cs
@@ -0,0 +1,11 @@
+﻿using CashBusiness.Domain.Entity;
+using FluentResults;
+
+namespace CashBusiness.Application.Services.Transaction.Commands;
+
+public interface ICashTransactionCommandService
+{
+    public Result<CashTransaction> PersistCashTransaction(string clientId, string voucher, string operationId,
+        double amount, string observation);
+    
+}
