b8cce208f88ade442bac83435cf5b42575c09ae6
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 12:23:40 -0400

    chore: customer vo created

---
+++ src/CashBusiness.Contracts/User/vo/CustomerVo.cs
@@ -0,0 +1,8 @@
+﻿namespace CashBusiness.Contracts.User.vo;
+
+public class CustomerVo
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+}
