f07200475b1f799464429fbe96895c5742755162
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 12:56:11 -0400

    chore: persist cash transaction repository method implemented

---
+++ src/CashBusiness.Infraestructure/Persistence/Transaction/CashTransactionRepositoryImpl.cs
@@ -0,0 +1,43 @@
+﻿using CashBusiness.Application.Common.Persistence.Transaction;
+using CashBusiness.Domain.Entity;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace CashBusiness.Infraestructure.Persistence.Transaction;
+
+public class CashTransactionRepositoryImpl: ICashTransactionRepository
+{
+    private readonly AppDbContext _dbContext;
+    
+    public CashTransactionRepositoryImpl(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+
+    public async Task<CashTransaction> PersistCashTransaction(string clientId, string voucher, string operationId, double amount, string observation)
+    {
+        var cashTransaction = new CashTransaction
+        {
+            ClientId = clientId, 
+            Voucher = voucher, 
+            OperationId = operationId, 
+            Amount = amount, 
+            Observation = observation
+        };
+
+        EntityEntry<CashTransaction> savedTransaction = _dbContext.Add(cashTransaction);
+        await _dbContext.SaveChangesAsync();
+
+        return savedTransaction.Entity;
+    }
+
+    public Task<CashTransaction> FindCashTransactionById()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<CashTransaction>> FindAllTransactions()
+    {
+        throw new NotImplementedException();
+    }
+}
