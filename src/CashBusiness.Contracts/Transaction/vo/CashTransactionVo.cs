50552d5b8eb4e5dffb71c3ba761d19a3e0eca1da
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 12:43:47 -0400

    chore: cash transaction vo updated date field removed

--- src/CashBusiness.Contracts/Transaction/vo/CashTransactionVo.cs
+++ src/CashBusiness.Contracts/Transaction/vo/CashTransactionVo.cs
@@ -3,8 +3,7 @@
 public class CashTransactionVo
 {
     public Guid Id { get; set; } = Guid.Empty;
-    public DateTime DateTime { get; set; } = DateTime.Now;
-    
+
     public string ClientId { get; set; } = String.Empty;
     public string Voucher { get; set; } = String.Empty;
     public string OperationId { get; set; } = String.Empty;
@@ -12,5 +11,4 @@ public class CashTransactionVo
     public string Observation { get; set; } = String.Empty;
 
     public DateTime CreatedAt { get; set; } = DateTime.Now;
-    public DateTime UpdatedAt { get; set; } = DateTime.Now;
 }
