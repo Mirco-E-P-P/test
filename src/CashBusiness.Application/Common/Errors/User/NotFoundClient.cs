4f28762e2db3ba515971a4e6970c9a4eff43c14c
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 08:24:42 -0400

    chore: status code added to not found client error metadata

--- src/CashBusiness.Application/Common/Errors/User/NotFoundClient.cs
+++ src/CashBusiness.Application/Common/Errors/User/NotFoundClient.cs
@@ -1,16 +1,21 @@
-﻿using FluentResults;
+﻿using System.Net;
+using FluentResults;
 
 namespace CashBusiness.Application.Common.Errors.User;
 
 public class NotFoundClient: IError
 {
     public string Message { get; }
-    public Dictionary<string, object> Metadata { get; }= new Dictionary<string, object>();
+    public Dictionary<string, object> Metadata { get; } = new Dictionary<string, object>();
+    
     public List<IError> Reasons { get; } = new List<IError>();
+
 
     public NotFoundClient(string message)
     {   
         Message = message;
+        Metadata.Add("statusCode", HttpStatusCode.NotFound);
+
     }
 
 }
