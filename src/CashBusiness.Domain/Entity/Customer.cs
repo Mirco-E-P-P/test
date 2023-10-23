1abb8fbacfccd1c3977398aeb1312b063858b62e
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 10:40:54 -0400

    refactor: client entity name change to customer

--- src/CashBusiness.Domain/Entity/Customer.cs
+++ src/CashBusiness.Domain/Entity/Customer.cs
@@ -1,10 +1,9 @@
 namespace CashBusiness.Domain.Entity;
 
-public class Client
+public class Customer
 {
     public int Index { get; set; } = 0;
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = string.Empty;
     public string PhoneNumber { get; set; } = string.Empty;
-
 }
