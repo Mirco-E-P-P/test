70ddc458c8997aa5ac1f53cee6e551995509c645
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 11:59:08 -0400

    chore: the index and id fields are the composite key of operation entity

--- src/CashBusiness.Infraestructure/Common/DbContext/config/OperationConfig.cs
+++ src/CashBusiness.Infraestructure/Common/DbContext/config/OperationConfig.cs
@@ -8,6 +8,7 @@ public class OperationConfig: IEntityTypeConfiguration<Operation>
 {
     public void Configure(EntityTypeBuilder<Operation> builder)
     {
-        builder.HasKey(operation => operation.Id);
+        builder.HasKey(nameof(Operation.Index), nameof(Operation.Id));
+        builder.Property(operation => operation.Index).ValueGeneratedOnAdd();
     }
 }
