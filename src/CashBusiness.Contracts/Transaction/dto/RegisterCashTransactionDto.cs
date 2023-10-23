24a18d4ba4c8f1efb574f0a7178c8e99e94b6d8c
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 15:06:12 -0400

    chore: userId property changed to customer id

--- src/CashBusiness.Contracts/Transaction/dto/RegisterCashTransactionDto.cs
+++ src/CashBusiness.Contracts/Transaction/dto/RegisterCashTransactionDto.cs
@@ -2,10 +2,9 @@
 
 public class RegisterCashTransactionDto
 {
-    public DateTime Date { get; set; } = DateTime.Now;
-    public string UserId { get; set; } = String.Empty;
-    public string Voucher{ get; set; } = String.Empty;
-    public string OperationId{ get; set; } = String.Empty;
+    public string CustomerId { get; set; } = String.Empty;
+    public string Voucher { get; set; } = String.Empty;
+    public string OperationId { get; set; } = String.Empty;
     public double Amount { get; set; } = 0.0;
     public string Observation { get; set; } = String.Empty;
 }
