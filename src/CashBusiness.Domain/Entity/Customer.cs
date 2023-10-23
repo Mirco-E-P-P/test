d416cf92ffd4bf5925c3781c770e7b1600fef7f9
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 15:03:58 -0400

    refactor: default int index value removed from customer entity

--- src/CashBusiness.Domain/Entity/Customer.cs
+++ src/CashBusiness.Domain/Entity/Customer.cs
@@ -3,7 +3,7 @@ namespace CashBusiness.Domain.Entity;
 public class Customer
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public int Index { get; set; } = 0;
+    public int Index { get; set; }
     public string Name { get; set; } = string.Empty;
     public string PhoneNumber { get; set; } = string.Empty;
 }
