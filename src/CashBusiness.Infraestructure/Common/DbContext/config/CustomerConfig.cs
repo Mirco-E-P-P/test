0ed0deb49befb5814ea108c82305ad87fdde5c96
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 10:11:19 -0400

    refactor: client config name change to customer config

--- src/CashBusiness.Infraestructure/Common/DbContext/config/CustomerConfig.cs
+++ src/CashBusiness.Infraestructure/Common/DbContext/config/CustomerConfig.cs
@@ -4,9 +4,9 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace CashBusiness.Infraestructure.config;
 
-public class ClientConfig: IEntityTypeConfiguration<Client>
+public class ClientConfig: IEntityTypeConfiguration<Customer>
 {
-    public void Configure(EntityTypeBuilder<Client> builder)
+    public void Configure(EntityTypeBuilder<Customer> builder)
     {
         builder.HasKey(client => client.Id);
         builder.Property(client => client.Name).IsRequired();
