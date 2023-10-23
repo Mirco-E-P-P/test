a74134e87e562a1d7f3f567f04e30034792148f0
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 11:52:14 -0400

    chore: index field added to customer entity

--- src/CashBusiness.Domain/Entity/Customer.cs
+++ src/CashBusiness.Domain/Entity/Customer.cs
@@ -2,8 +2,8 @@ namespace CashBusiness.Domain.Entity;
 
 public class Customer
 {
-    public int Index { get; set; } = 0;
     public Guid Id { get; set; } = Guid.NewGuid();
+    public int Index { get; set; } = 0;
     public string Name { get; set; } = string.Empty;
     public string PhoneNumber { get; set; } = string.Empty;
 }
