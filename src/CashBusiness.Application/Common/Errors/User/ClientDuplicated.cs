5f6d8e5ff499e7b82b8cbbb443d3df31b1419bad
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 08:26:47 -0400

    chore: status code added to duplicated client error metadata

--- src/CashBusiness.Application/Common/Errors/User/ClientDuplicated.cs
+++ src/CashBusiness.Application/Common/Errors/User/ClientDuplicated.cs
@@ -1,16 +1,19 @@
-﻿using FluentResults;
+﻿using System.Net;
+using FluentResults;
 
 namespace CashBusiness.Application.Common.Errors.User;
 
 public class ClientDuplicated: IError
 {
     public string Message { get; }
-    public Dictionary<string, object> Metadata { get; }= new Dictionary<string, object>();
+    public Dictionary<string, object> Metadata { get; } = new Dictionary<string, object>();
+    
     public List<IError> Reasons { get; }= new List<IError>();
 
     public ClientDuplicated(string message)
     {
         Message = message;
+        Metadata.Add( "statusCode", HttpStatusCode.Conflict);
     }
 
 }
