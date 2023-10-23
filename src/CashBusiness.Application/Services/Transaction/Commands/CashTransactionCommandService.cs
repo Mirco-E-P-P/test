c4a6dd3aa81bcf10d47feee3896c822561ba5acf
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 13:07:27 -0400

    chore: persist cash transaction service method implemented

---
+++ src/CashBusiness.Application/Services/Transaction/Commands/CashTransactionCommandService.cs
@@ -0,0 +1,23 @@
+﻿using CashBusiness.Application.Common.Persistence.Transaction;
+using CashBusiness.Domain.Entity;
+using FluentResults;
+
+namespace CashBusiness.Application.Services.Transaction.Commands;
+
+public class CashTransactionCommandService: ICashTransactionCommandService
+{
+    private readonly ICashTransactionRepository _repository;
+
+    public CashTransactionCommandService(ICashTransactionRepository repository)
+    {
+        _repository = repository;
+    }
+    
+    public async Task<Result<CashTransaction>> PersistCashTransaction(string clientId, string voucher, string operationId, double amount, string observation)
+    {
+        CashTransaction savedTransaction = await 
+            _repository.PersistCashTransaction(clientId, voucher, operationId, amount, observation);
+
+        return Result.Ok(savedTransaction);
+    }
+}
