7f37f9803ac234e87a02fd2f4d9b343278450568
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-23 11:52:45 -0400

    chore: index field added to operation entity

--- src/CashBusiness.Domain/Entity/Operation.cs
+++ src/CashBusiness.Domain/Entity/Operation.cs
@@ -1,5 +1,6 @@
 namespace CashBusiness.Domain.Entity;
 public class Operation {
+    public int Index { get; set; } = 0;
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = String.Empty;
 
