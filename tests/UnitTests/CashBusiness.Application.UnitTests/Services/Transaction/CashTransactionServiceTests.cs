3c359509f94bc5a4f51031fe1bb72fa4426e0c29
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-24 10:49:11 -0400

    test: get registered transaction by id test

---
+++ tests/UnitTests/CashBusiness.Application.UnitTests/Services/Transaction/CashTransactionServiceTests.cs
@@ -0,0 +1,57 @@
+using CashBusiness.Application.Common.Persistence.Transaction;
+using CashBusiness.Application.Common.Persistence.user;
+using CashBusiness.Application.Services.Transaction.Queries;
+using CashBusiness.Application.Services.User.Queries;
+using CashBusiness.Application.UnitTests.Services.Transaction.ServiceUtils;
+using CashBusiness.Domain.Entity;
+using CashBusiness.Infraestructure.Persistence;
+using CashBusiness.Infraestructure.Persistence.Transaction;
+using CashBusiness.Infraestructure.Persistence.User;
+using FluentResults;
+
+namespace CashBusiness.Application.UnitTests.Services.Transaction;
+
+public class CashTransactionServiceTests
+{
+    [Fact]
+    public async Task CashTransactionQueryService_GetRegisteredTransactionById_ShouldReturnTransaction()
+    {
+        // Arrange
+        AppDbContext dbContext = InMemoryApplicationDatabase.GetInstance().GetApplicationDbContext();
+        ICashTransactionRepository cashTransactionRepository = new CashTransactionRepositoryImpl(dbContext);
+        ICashTransactionQueryService cashTransactionQueryService = new CashTransactionQueryService(cashTransactionRepository);
+        
+        Guid newTransactionId = new Guid("58C8311B-244E-40D4-92F2-283F01B581A9");
+        
+        CashTransaction newTransaction = new CashTransaction()
+        {
+            Id = newTransactionId,
+            CustomerId = new Guid("25d65bb9-de48-472e-8feb-c71cb59f40ac"),
+            Voucher = "000000001",
+            OperationId = new Guid("9C30807B-5188-4348-B89F-69854029913C"),
+            Amount = 999.15,
+            Observation = "None"
+        };
+        
+        dbContext.CashTransactions.Add(newTransaction);
+        await dbContext.SaveChangesAsync();
+        
+        // Act
+        Result <CashTransaction> result = await cashTransactionQueryService.GetTransactionById(newTransactionId);
+        
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal(newTransactionId, result.Value.Id);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
