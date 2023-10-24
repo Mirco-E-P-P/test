793e44d5a52a593bab24774d5dc07b0f66f586b3
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-24 12:23:35 -0400

    chore: update cash transaction dto added

---
+++ src/CashBusiness.Contracts/Transaction/dto/UpdateCashTransactionDto.cs
@@ -0,0 +1,11 @@
+namespace CashBusiness.Contracts.Transaction.dto;
+
+public class UpdateCashTransactionDto
+{
+     public Guid Id { get; set; }
+     public Guid CustomerId { get; set; }
+     public Guid OperationId { get; set; }
+     public string Voucher { get; set; } = String.Empty;
+     public double Amount { get; set; } = 0.0;
+     public string Observation { get; set; } = String.Empty;
+}
