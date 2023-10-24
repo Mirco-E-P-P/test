420ad9b8065b5beeebea3db1749a8a2933a78272
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-24 15:29:34 -0400

    refactor: OperationId type changed to Guid in RegisterCashTransaction request

--- src/CashBusiness.Contracts/Transaction/dto/RegisterCashTransactionDto.cs
+++ src/CashBusiness.Contracts/Transaction/dto/RegisterCashTransactionDto.cs
@@ -4,8 +4,8 @@ public class RegisterCashTransactionDto
 {
     public Guid Id { get; set; }
     public Guid CustomerId { get; set; }
+    public Guid OperationId { get; set; }
     public string Voucher { get; set; } = String.Empty;
-    public string OperationId { get; set; } = String.Empty;
     public double Amount { get; set; } = 0.0;
     public string Observation { get; set; } = String.Empty;
 }
