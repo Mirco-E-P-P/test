bc6174af3b4d585e0175ab18471aefc2ca3c5529
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-24 10:08:34 -0400

    chore: operation registered validation added to RegisterCashTransaction method

--- src/CashBusiness.Api/Controllers/Transaction/CashTransactionController.cs
+++ src/CashBusiness.Api/Controllers/Transaction/CashTransactionController.cs
@@ -1,4 +1,5 @@
 ﻿using CashBusiness.Application.Services.Transaction.Commands;
+using CashBusiness.Application.Services.Transaction.Queries;
 using CashBusiness.Application.Services.User.Queries;
 using CashBusiness.Contracts.Transaction.dto;
 using CashBusiness.Contracts.Transaction.vo;
@@ -16,34 +17,43 @@ public class CashTransactionController: ControllerBase
 {
     private readonly ICashTransactionCommandService _cashTransactionCommandService;
     private readonly ICustomerQueryService _customerQueryService;
+    private readonly IOperationQueryService _operationQueryService;
     private readonly IMapper _mapper;
 
     public CashTransactionController(
         ICashTransactionCommandService cashTransactionCommandService, 
         ICustomerQueryService customerQueryService, 
+        IOperationQueryService operationQueryService,
         IMapper mapper)
     {
         _cashTransactionCommandService = cashTransactionCommandService;
         _customerQueryService = customerQueryService;
+        _operationQueryService = operationQueryService;
         _mapper = mapper;
     }
     
     [HttpPost]
-    public async Task<IActionResult> Post(RegisterCashTransactionDto dto)
+    public async Task<IActionResult> RegisterCashTransaction(RegisterCashTransactionDto dto)
     {
         Result <Customer> customerResult = await _customerQueryService.FindCustomerById(dto.CustomerId);
         
         if (customerResult.IsFailed)
         {
-            IError firstError = customerResult.Errors[0];
-            return Problem(title: firstError.Message, statusCode:(int) firstError.Metadata["status"]);
+            IError firstCustomerResultErrorError = customerResult.Errors[0];
+            return Problem(title: firstCustomerResultErrorError.Message, statusCode:(int) firstCustomerResultErrorError.Metadata["statusCode"]);
         }
-        
-        
+
+        Result<Operation> operationResult = await _operationQueryService.FindOperationByIdAsync(dto.OperationId);
+
+        if (operationResult.IsFailed)
+        {
+            IError firstOperationResultError = operationResult.Errors[0];
+            return Problem(title: firstOperationResultError.Message, statusCode:(int) firstOperationResultError.Metadata["statusCode"]);
+        }
+
+
         CashTransaction cashTransaction = _mapper.Map<CashTransaction>(dto);
         Result<CashTransaction> transactionResult = await _cashTransactionCommandService.PersistCashTransaction(cashTransaction);
-        
-        
         return Ok(_mapper.Map<CashTransactionVo>(transactionResult.Value));
     }
     
