d9d39cf25f7c7b3f08802316e654715932f4f56b
Mirco-E-P-P <mircopasantia@gmail.com>
2023-10-24 15:55:27 -0400

    refactor: DeleteCashTransaction method return the number of affected rows

--- src/CashBusiness.Application/Common/Persistence/Transaction/ICashTransactionRepository.cs
+++ src/CashBusiness.Application/Common/Persistence/Transaction/ICashTransactionRepository.cs
@@ -4,10 +4,9 @@ namespace CashBusiness.Application.Common.Persistence.Transaction;
 
 public interface ICashTransactionRepository
 {
-    
     public Task<CashTransaction> PersistCashTransaction(CashTransaction cashTransaction);
     public Task<CashTransaction> FindCashTransactionById(Guid id);
     public Task<List<CashTransaction>> FindAllTransactions();
     public Task<CashTransaction> UpdateCashTransaction(CashTransaction cashTransaction);
-    public Task DeleteCashTransactionById(Guid id);
+    public Task<int> DeleteCashTransactionById(Guid id);
 }
